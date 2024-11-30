45956bdba4684c1253c66b9993717485a65d439d
Li824498 <1691508029@qq.com>
2024-11-30 13:35:18 +0800

    2024/11/30

--- leetcode-learn/Arrays/src/L3HashMap/Leet5_15.java
+++ leetcode-learn/Arrays/src/L3HashMap/Leet5_15.java
@@ -36,16 +36,15 @@ public class Leet5_15 {
                         left++;
                     } else {
                         result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                        while((left<right)&&(nums[left] == nums[left+1])) {
+                        left++;
+                        right--;
+                        while((left<right)&&(nums[left] == nums[left-1])) {
                             left++;
                         }
-                        while((left<right)&&(nums[right] == nums[right-1])) {
+                        while((left<right)&&(nums[right] == nums[right+1])) {
                             right--;
                         }
 
-                        left++;
-                        right--;
                     }
                 }
 
